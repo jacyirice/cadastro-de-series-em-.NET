d383c49be930248f696bf9e8cf041435939e0ab3
Jacyiricê Silva Oliveira <jacyirice@gmail.com>
2021-09-08 06:32:29 -0300

    Adicionando atributo Excluido

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -9,6 +9,7 @@ namespace DIO.Series
         private string Titulo { get; set; }
         private string Descricao { get; set; }
         private int Ano { get; set; }
+        private bool Excluido { get; set; }
 
         // Metodos
         public Serie(int id, Genero genero, string titulo, string descricao, int ano)
@@ -18,26 +19,32 @@ namespace DIO.Series
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+            this.Excluido = false;
         }
 
         public override string ToString()
-		{
+        {
             string retorno = "";
             retorno += "Gênero: " + this.Genero + Environment.NewLine;
             retorno += "Titulo: " + this.Titulo + Environment.NewLine;
             retorno += "Descrição: " + this.Descricao + Environment.NewLine;
             retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
-			return retorno;
-		}
+            return retorno;
+        }
         public string retornaTitulo()
-		{
-			return this.Titulo;
-		}
+        {
+            return this.Titulo;
+        }
 
-		public int retornaId()
-		{
-			return this.Id;
-		}
+        public int retornaId()
+        {
+            return this.Id;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
     }
 
 
